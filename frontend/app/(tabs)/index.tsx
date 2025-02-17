762ac5db1800deae7514c00ec569882f7053876c
bbxx77 <220842@astanait.edu.kz>
2025-02-17 20:08:42 +0500

    index.js moved

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import api from '../api'; // Убедись, что путь правильный
+import api from '../../api'; 
 
 export default function Index() {
   const [username, setUsername] = useState('');
